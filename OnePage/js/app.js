a990454d691b207fcb49c02174478b3d22f6a6b3
hemdagem <hemdagem@gmail.com>
2013-12-28 14:57:59 +0000

    removed mvc project files

--- OnePage/js/app.js
+++ OnePage/js/app.js
@@ -3,62 +3,120 @@
 /*global $ */
 function CardsController($scope) {
     "use strict";
-    
-    $scope.stories = [];
-    var index = 0, stories = [];
-    
+
+    $scope.board = {};
+    $scope.board.cards = [];
+    $scope.board.cards.stories = [];
+
+    var index = 0;
+
     function updateLocalStorage() {
         if (window.localStorage) {
-            window.localStorage.setItem("stories", JSON.stringify($scope.stories));
+            window.localStorage.setItem("board", JSON.stringify($scope.board));
         }
     }
-    
-    if (window.localStorage && window.localStorage.getItem("stories")) {
-        
-        $scope.stories = JSON.parse(window.localStorage.getItem("stories"));
+
+    if (window.localStorage && window.localStorage.getItem("board")) {
+
+        $scope.board = JSON.parse(window.localStorage.getItem("board"));
     }
-    
+
+
+    if (!$scope.board.cards || $scope.board.cards.length == 0) {
+
+        $scope.board.cards = [];
+
+        $scope.board.cards.push({
+            title: "Ready",
+            status: 1,
+            stories: []
+        },
+        {
+            title: "Commit",
+            status: 2,
+            stories: []
+        },
+        {
+            title: "In Progress",
+            status: 3,
+            stories: []
+        },
+        {
+            title: "QA",
+            status: 4,
+            stories: []
+        },
+        {
+            title: "Demo",
+            status: 5,
+            stories: []
+        });
+
+    }
+
     $scope.clearBoard = function () {
-        $scope.stories = [];
+        $scope.board.stories = [];
         updateLocalStorage();
     };
-    
-    $scope.updateStory = function (id, title) {
-        
+
+
+    $scope.setPosition = function (id, status) {
+
         angular.forEach($scope.stories, function (story) {
             if (story.$$hashKey === id) {
-                story.title = title;
+                story.status = status;
                 return;
             }
         });
-        
+
+        updateLocalStorage();
+    }
+
+    $scope.updateStory = function (id, title) {
+
+        angular.forEach($scope.board.cards, function (card) {
+
+            angular.forEach(card.stories, function (story) {
+                if (story.$$hashKey === id) {
+                    story.title = title;
+                    return;
+                }
+            });
+
+        });
+
         updateLocalStorage();
     };
-    
-    $scope.editStory = function ($editData) {
-    
-    
-        $('#myModal').modal('show');
-    };
-    
+
+
     $scope.addStory = function () {
-        $scope.stories.push({
+
+        var card = $scope.board.cards[parseInt($scope.status) - 1];
+
+        card.stories.push({
             title: $scope.title,
             points: $scope.points,
             status: $scope.status,
             criteria: $scope.criteria
         });
-        
+
         updateLocalStorage();
-        $scope.form.$setPristine();
         $('#myModal').modal('hide');
     };
+
     
-    $(".list-group").sortable({
-        connectWith: ".list-group",
-        placeholder: "ui-state-highlight",
-        dropOnEmpty: true,
-        handle : '.glyphicon-move'
+    angular.element(document).ready(function () {
+        $(".list-group").sortable({
+            connectWith: ".list-group",
+            placeholder: "ui-state-highlight",
+            dropOnEmpty: true,
+            handle: '.glyphicon-move',
+            update: function (event, ui) {
+                $scope.$digest();
+                updateLocalStorage();
+            }
+        });
+
     });
 }
 
@@ -70,7 +128,7 @@ angular.module('scrumBoard', []).directive('contenteditable', function () {
             // view -> model
             elm.on('blur', function () {
                 scope.$apply(function () {
-                    scope.updateStory(scope.story.$$hashKey, elm.html());
+                    scope.updateStory(scope.board.story.$$hashKey, elm.html());
                 });
             });
         }
